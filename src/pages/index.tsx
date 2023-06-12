4c6d34b137d276c785ec660eb8c49a50ef060053
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2023-06-12 15:27:57 +0800

    chatroom start

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="mx-auto bg-stone-200 rounded-xl shadow-xl my-[2rem] hover:scale-105 select-none duration-300 w-[40rem] py-[10rem] text-center text-5xl font-bold">
+      Hello chatroom
+    </div>
+  )
+}
